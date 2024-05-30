7892d992430a1193dfca4e8b51cd03e49f7aae5c
RodPC <rodri_08romero@hotmail.com>
2024-05-30 11:13:28 -0300

    Codigos de trabajo de Debugging

---
+++ TrabajoDebugging/src/EJC/ProductController.java
@@ -0,0 +1,24 @@
+package EJC;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private List<Product> products = new ArrayList<>();
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return products;
+    }
+
+    @PostMapping
+    public String addProduct(@RequestBody Product product) {
+        products.add(product);
+        return "Product added successfully";
+    }
+}
+
